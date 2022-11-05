83d039731128e4635877c3256b3f3a20fd56d166
james.anderson <james.anderson@chrysos.com.au>
2022-11-05 18:49:55 +1030

    main: add book_item.py

---
+++ mini_projects/classes/book_item/book_item.py
@@ -0,0 +1,8 @@
+class Book:
+    title = "Harry Potter and the Deathly Hallows"
+    pages = 607
+    number = "7th"
+    genre = "fantasy"
+    print(f"{title} has {pages} pages")
+    print(f"It is the {number} in the series.")
+
