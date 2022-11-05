ec2912f22cf6236b7ed2bb3662689dd559527eae
james.anderson <james.anderson@chrysos.com.au>
2022-11-05 23:30:02 +1030

    main: add archery_game.py

---
+++ mini_projects/object_oriented_programming/archery_game/archery_game.py
@@ -0,0 +1,22 @@
+class Archer:
+    arrows = 10
+    points = 0
+    empty = False
+
+    def shoot(self):
+        if self.arrows > 0:
+            print('Aim... and shoot!')
+            self.arrows -= 1
+        else:
+            self.empty = True
+
+    def bullseye(self):
+        self.points += 3
+
+
+    def reload(self):
+        if self.empty == True:
+            self.arrows = 10
+            self.empty = False
+        else:
+            print(f"Still have {self.arrows} left - reload at 0")
