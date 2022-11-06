2f8a035f51601d986758f8e76b8ba76ddfa53344
james.anderson <james.anderson@chrysos.com.au>
2022-11-06 15:12:58 +1030

    main: add calorie_calculator.py

---
+++ mini_projects/errors_and_exceptions/calorie_calculator.py
@@ -0,0 +1,22 @@
+# There are multiple syntax errors in this code snippet
+# Can you find them all? - uncomment the snippet to work on it.
+#
+# calories = {"rice": ,
+# "bread": 300,
+# "curry":700,
+# "salad": "100",
+# "chutney": 100,
+# papadums - 200,}
+#
+#
+# meal = ["rice", curry, "salad", "chutney"]
+#
+# "total" == ""
+#
+#
+# for item of meal
+#     total +=calories(item)
+#
+#
+# print("Total calories in meal: " + total)
+
